dcc2a6612a84b99c8369e8f3f7ab4586cc8b50bc
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-07-09 05:02:47 -0600

    test add branch

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <div style="bottom: 0" class="container-fluid m-0 p-0 pt-5" >
     <div class="bg-dark text-light">
-        <p>Hahahahahhahahahahahah</p>
+        <p>Hahahahahhahahahahahah dsa12312321321</p>
     </div>
 </div>
